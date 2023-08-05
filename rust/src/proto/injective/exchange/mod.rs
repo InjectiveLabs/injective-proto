4d2d41dadf3e785140ffbc906c316782abef29bc
Phuc Ta <vinhphuctadang@gmail.com>
2023-08-06 00:50:37 +0700

    feat: initial commit with rust genreated proto

---
+++ rust/src/proto/injective/exchange/mod.rs
@@ -0,0 +1,8 @@
+// @generated
+
+pub mod tx;
+pub mod events;
+pub mod exchange;
+pub mod query;
+pub mod authz;
+pub mod genesis;
---
+++ rust/src/proto/injective/ocr/mod.rs
@@ -0,0 +1,6 @@
+// @generated
+
+pub mod tx;
+pub mod ocr;
+pub mod query;
+pub mod genesis;
